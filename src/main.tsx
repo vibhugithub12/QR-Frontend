1aadb3356dfdce10af827b7ee3692fd41ef88691
vibhugithub12 <vibhuchauhan37@gmail.com>
2026-01-08 16:37:10 +0530

    change

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App title="React QR Validator App" author='Vaibhav' age={21}/>
+    <App/>
   </StrictMode>,
 )
